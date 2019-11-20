615f2f3bf04911da51e33d05b11932b8ef9557c1
Gavril Ioan Hasmasan <gsylverman@gmail.com>
2019-11-20 12:13:47 +0200

    Add files via upload

--- src/Components/MoneyControl/MoneyControl.js
+++ src/Components/MoneyControl/MoneyControl.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import classes from "./MoneyControl.module.css";
 
-
 class MoneyControl extends Component {
 
     constructor(props) {
@@ -9,21 +8,22 @@ class MoneyControl extends Component {
         // ref pt focusare element
         this.amountInput = React.createRef();
     }
+
     componentDidMount() {
         //focuseaza pe amount la deschidere aplicatie
         this.amountInput.current.focus();
     }
+
     componentDidUpdate() {
         //focuseaza iar pe amount
-        if (this.props.amount < 10)
+        if (this.props.amount < 10) {
             setTimeout(() => {
                 this.amountInput.current.focus();
             }, 3000);
-
+        }
     }
 
     render() {
-
         const bitCoinStyle = {
             position: "absolute",
             top: "50%",
@@ -41,38 +41,29 @@ class MoneyControl extends Component {
         let disabled = this.props.started && this.props.amount < 10 ? false : this.props.started;
 
         return (
-
-
             <div className={classes.MoneyControl}>
-
                 <div className={classes.triangleLeft}></div>
-
                 <div className={classes.circle}>
                     <i style={bitCoinStyle} className="fab fa-btc"></i>
                 </div>
-
-
-
                 <input
                     ref={this.amountInput}
-                    placeholder="Enter Amount" className={classes.amount} disabled={disabled} type="number" onChange={(e) => this.props.changeAmount(e.target.value)} value={this.props.amount}></input>
+                    placeholder="Enter Amount"
+                    className={classes.amount}
+                    disabled={disabled}
+                    type="number"
+                    onChange={(e) => this.props.changeAmount(e.target.value)}
+                    value={this.props.amount}>
+                </input>
 
                 <div className={classes.triangleRight}></div>
                 <div className={classes.hex}></div>
-
                 <div className={startClasses}>
                     Start
-                    </div>
-
-
+                </div>
             </div>
-
-
         );
     }
-
-
 };
-
 
 export default MoneyControl;
